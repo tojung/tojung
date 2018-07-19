b2ec9f2c9ea1812b30bb3a72bc0188d52b0e2b8f
limdongjin <geniuslim27@gmail.com>
2018-07-19 17:22:09 +0900

    Change database.yml and puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -4,8 +4,12 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-threads threads_count, threads_count
+workers 2
+
+threads 1, 6
+
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/shared"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
@@ -21,7 +25,6 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
