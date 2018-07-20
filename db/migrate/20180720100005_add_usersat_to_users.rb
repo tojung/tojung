9b5d88c594ae2ae65017215e526d3bac2432caaf
limdongjin <geniuslim27@gmail.com>
2018-07-20 19:07:33 +0900

    User model method update

---
+++ db/migrate/20180720100005_add_usersat_to_users.rb
@@ -0,0 +1,7 @@
+class AddUsersatToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+    add_column :users, :image, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180720080256) do
+ActiveRecord::Schema.define(version: 20180720100005) do
 
   create_table "identities", force: :cascade do |t|
     t.integer "user_id"
@@ -42,6 +42,9 @@ ActiveRecord::Schema.define(version: 20180720080256) do
     t.string "name"
     t.text "address"
     t.text "phone_number"
+    t.string "provider"
+    t.string "uid"
+    t.text "image"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
