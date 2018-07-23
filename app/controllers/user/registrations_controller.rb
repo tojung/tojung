d71c08162e43c34e406154bc4b474f9826da5810
limdongjin <geniuslim27@gmail.com>
2018-07-23 17:23:42 +0900

    로그인 기능 구현 완료

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -42,7 +42,7 @@ class User::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_sign_up_params
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :phone_number])
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :phone_number, :address_num, :address_text, :address_extra])
    end
 
   # If you have extra params to permit, append them to the sanitizer.
