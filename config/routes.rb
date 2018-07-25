c91860c78225b83642e9479c66d7afd24e6bcc52
limdongjin <geniuslim27@gmail.com>
2018-07-25 16:58:20 +0900

    Refactoring codes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,22 @@
 Rails.application.routes.draw do
   require 'sidekiq/web'
-  
+
   root 'home#index'
 
   get 'home/index'
   get 'home/about'
-  
+
   get 'additional_info' => 'userupdaters#additional'
   post 'additional_update' => 'userupdaters#additional_update'
 
-  devise_for :users, :controllers => { 
+  devise_for :users, controllers: {
     omniauth_callbacks: 'user/omniauth_callbacks',
     registrations: 'user/registrations'
-  } 
-  
-  authenticate :user, lambda { |u| u.admin } do
+  }
+
+  authenticate(:user, ->(u) do
+    u.admin
+  end) do
     mount Sidekiq::Web => '/sidekiq'
   end
 end
