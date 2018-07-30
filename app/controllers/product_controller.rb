c9a8fa41c1fe41f6ea46942493a4424074e06687
limdongjin <30315743+limdongjin@users.noreply.github.com>
2018-07-30 21:33:28 +0900

    Product 생성페이지1 및 Detail 페이지 구조 잡음 (#7)

---
+++ app/controllers/product_controller.rb
@@ -0,0 +1,47 @@
+class ProductController < ApplicationController
+  before_action :check_admin, only: [:new, :create, :detail_ready]
+
+  # GET '/product/new'
+  def new; end
+
+  # POST '/product/create'
+  def create
+    product = Product.new
+    product.createNew(product_params)
+
+    redirect_to '/product/ready/' + product.id.to_s
+  end
+
+  # GET '/product/#{num}'
+  def detail
+    # 이 페이지는 redis 적용 예정
+    @product = Product.find(params[:product_id])
+    redirect_to '/' unless @product.visible
+  end
+
+  # GET '/product/ready/#{num}'
+  def detail_ready
+    # 일반 유저에게 배포하기전 페이지
+    @product = Product.find(params[:product_id])
+  end
+
+  private
+
+  def check_admin
+    redirect_to '/' if !user_signed_in? || !current_user.admin
+  end
+
+  def product_params
+    params.permit(:title,
+                  :subtitle,
+                  :start_date,
+                  :end_date,
+                  :youtb_url,
+                  :youtb_content,
+                  :bill_url,
+                  :assos,
+                  :fb_url,
+                  :image0,
+                  :goal_money)
+  end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,21 @@
 class Product < ApplicationRecord
+  mount_uploader :image0, ImageUploader
+  mount_uploader :image1, ImageUploader
+
+  def createNew(params)
+    print(params)
+    self.name    = params[:title]
+    self.subname = params[:subtitle]
+    self.start_date = params[:start_date]
+    self.end_date = params[:end_date]
+    self.youtb_url = params[:youtb_url]
+    self.video_text = params[:youtb_content]
+    self.bill_url = params[:bill_url]
+    self.assos = params[:assos]
+    self.category = self.assos
+    self.fb_url = params[:fb_url]
+    self.image0 = params[:image0]
+    self.goal_money = params[:goal_money]
+    self.save
+  end
 end
