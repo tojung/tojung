87a00c0f48179c9a53e35893853b6255e5f6aa33
limdongjin <geniuslim27@gmail.com>
2018-07-30 21:31:40 +0900

    Product 생성페이지1 및 Detail 페이지 구조 잡음

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,12 @@ Rails.application.routes.draw do
   post 'additional_update' => 'userupdaters#additional_update'
 
   get 'mainimage/new'
-
   post 'mainimage/create'
+
+  get 'product/new'
+  post 'product/create'
+  get 'product/:product_id' => 'product#detail'
+  get 'product/ready/:product_id' => 'product#detail_ready'
 
   devise_for :users, controllers: {
     omniauth_callbacks: 'user/omniauth_callbacks',
