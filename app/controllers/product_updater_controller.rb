f1761b171746ae219dcfbc269dd6fd95195030a9
limdongjin <geniuslim27@gmail.com>
2018-07-30 23:14:58 +0900

    상품 수정 페이지 ( 기본 정보, 이미지 )

---
+++ app/controllers/product_updater_controller.rb
@@ -0,0 +1,66 @@
+class ProductUpdaterController < ApplicationController
+  before_action :check_admin
+  before_action :find_product
+
+  # GET 'product/edit/:product_id'
+  def edit
+  end
+
+  # POST '/product/update/:product_id'
+  def update
+    @product.updateNormal(update_normal_params)
+
+    redirect_to '/product/ready/' + @product.id.to_s
+  end
+
+  # GET '/product/image0/:product_id'
+  def imagedit; end
+
+  # GET '/product/image1/:product_id'
+  def imagedit2; end
+
+  # POST '/product/image0/:product_id'
+  def update_image0
+    # 메인 이미지
+    @product.updateImage0(update_image0_params)
+
+    redirect_to '/product/ready/' + @product.id.to_s
+  end
+
+  # POST '/product/image1/:product_id'
+  def update_image1
+    # 패키지 정리 이미지
+    @product.updateImage1(update_image1_params)
+
+    redirect_to '/product/ready/' + @product.id.to_s
+  end
+
+  private
+
+  def check_admin
+    redirect_to '/' if !user_signed_in? || !current_user.admin
+  end
+
+  def find_product
+    @product = Product.find(params[:product_id])
+  end
+
+  def update_normal_params
+    params.permit(
+      :title, :subtitle,
+      :youtb_url,
+      :youtb_content,
+      :assos,
+      :fb_url,
+      :goal_money, :bill_url
+    )
+  end
+
+  def update_image0_params
+    params.permit(:image0)
+  end
+
+  def update_image1_params
+    params.permit(:image1)
+  end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,6 @@ class Product < ApplicationRecord
   mount_uploader :image1, ImageUploader
 
   def createNew(params)
-    print(params)
     self.name    = params[:title]
     self.subname = params[:subtitle]
     self.start_date = params[:start_date]
@@ -16,6 +15,29 @@ class Product < ApplicationRecord
     self.fb_url = params[:fb_url]
     self.image0 = params[:image0]
     self.goal_money = params[:goal_money]
+    self.save
+  end
+
+  def updateNormal(params)
+    self.name = params[:title]
+    self.subname = params[:subtitle]
+    self.youtb_url = params[:youtb_url]
+    self.video_text = params[:youtb_content]
+    self.assos = params[:assos]
+    self.category = self.assos
+    self.fb_url = params[:fb_url]
+    self.goal_money = params[:goal_money]
+    self.bill_url = params[:bill_url]
+    self.save
+  end
+
+  def updateImage0(params)
+    self.image0 = params[:image0]
+    self.save
+  end
+
+  def updateImage1(params)
+    self.image1 = params[:image1]
     self.save
   end
 end
