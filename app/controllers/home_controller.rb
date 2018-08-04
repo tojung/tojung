9ca2f5498561798755b648aff6eb904150a8a8d2
limdongjin <geniuslim27@gmail.com>
2018-08-04 16:20:40 +0900

    Ref

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,9 @@
 class HomeController < ApplicationController
   before_action :require_more_data, :check_admin
-  before_action :image_read_or_set, :products_read,:set_cdn_url, only: [:index]
-  
+  before_action :image_read_or_set, :products_read, :set_cdn_url, only: [:index]
+
   # GET '/' 메인 페이지
-  def index
-  end
+  def index; end
 
   # GET '/about' about 페이지
   def about; end
@@ -20,21 +19,21 @@ class HomeController < ApplicationController
   end
 
   def image_read_or_set
-     mainimage0 = $redis.get('mainimage0')
-     if mainimage0 == nil 
-       mainimages = Mainimage.last
-       if mainimages == nil 
-         redirect_to '/mainimage/new'
-       else 
-         mainimages.setRedis
-       end
-     else
-         @mainimage0 = $redis.get('mainimage0')
-         @mainimage1 = $redis.get('mainimage1')
-         @mainimage2 = $redis.get('mainimage2')
-     end
+    mainimage0 = $redis.get('mainimage0')
+    if mainimage0 == nil
+      mainimages = Mainimage.last
+      if mainimages == nil
+        redirect_to '/mainimage/new'
+      else
+        mainimages.setRedis
+      end
+    else
+      @mainimage0 = $redis.get('mainimage0')
+      @mainimage1 = $redis.get('mainimage1')
+      @mainimage2 = $redis.get('mainimage2')
+    end
   end
-  
+
   def products_read
     # Redis 적용 예정
     @products = Product.where(visible: [true, 1])
