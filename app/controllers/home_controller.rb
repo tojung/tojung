39f665229d73fb122fffee0051da4e7c8a2554aa
limdongjin <geniuslim27@gmail.com>
2018-08-20 17:34:42 +0900

    Refactoring Home Controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,59 +1,63 @@
 class HomeController < ApplicationController
   before_action :require_more_data, :check_admin, :set_cdn_url
   before_action :image_read_or_set, :products_read, :set_cdn_url, only: [:index]
-  before_action :set_cdn_url, only: [:index, :policy, :privacy, :brand]
+  before_action :set_cdn_url, only: %i[index policy privacy brand]
   # GET '/' 메인 페이지
   def index
-    if params.has_key?(:sidebar)
-      @num=params[:sidebar]
-      if (@num == '1')
-        @products = @products.where(category: "인권/성평등")
+    if params.key?(:sidebar)
+      @num = params[:sidebar]
+      if @num == '1'
+        @products = @products.where(category: '인권/성평등')
       elsif @num == '2'
-        @products = @products.where(category: "동물")
+        @products = @products.where(category: '동물')
       elsif @num == '3'
-        @products = @products.where(category: "육아/교육")
+        @products = @products.where(category: '육아/교육')
       elsif @num == '4'
-        @products = @products.where(category: "안전/환경")
+        @products = @products.where(category: '안전/환경')
       elsif @num == '5'
-        @products = @products.where(category: "보건/복지")
+        @products = @products.where(category: '보건/복지')
       elsif @num == '6'
-        @products = @products.where(category: "외교/통일/국방")
+        @products = @products.where(category: '외교/통일/국방')
       end
     end
   end
-
 
   def categorize
     @cdn_url = 'https://d1eq7v76s8dt2n.cloudfront.net/'
     @products = Product.where(visible: [true, 1])
     @num = params[:category_id]
-    if (@num == '1')
-      @products_categorized  = @products.where(category: "인권/성평등")
+    if @num == '1'
+      @products_categorized = @products.where(category: '인권/성평등')
     elsif @num == '2'
-      @products_categorized = @products.where(category: "동물")
+      @products_categorized = @products.where(category: '동물')
     elsif @num == '3'
-      @products_categorized = @products.where(category: "육아/교육")
+      @products_categorized = @products.where(category: '육아/교육')
     elsif @num == '4'
-      @products_categorized = @products.where(category: "안전/환경")
+      @products_categorized = @products.where(category: '안전/환경')
     elsif @num == '5'
-      @products_categorized = @products.where(category: "보건/복지")
+      @products_categorized = @products.where(category: '보건/복지')
     elsif @num == '6'
-      @products_categorized = @products.where(category: "외교/통일/국방")
+      @products_categorized = @products.where(category: '외교/통일/국방')
 
-    respond_to do |format|
-      format.js { render :locals => { :products_categorized => @products_categorized } }
-    end
+      respond_to do |format|
+        format.js { render locals: { products_categorized: @products_categorized } }
+      end
     end
   end
 
   def sidebar_categorize
     redirect_to root_path(sidebar: params[:id])
   end
+
   # GET '/about' about 페이지
   def about; end
+
   def privacy; end
+
   def policy; end
+
   def brand; end
+
   private
 
   def image_read_or_set
@@ -77,10 +81,9 @@ class HomeController < ApplicationController
     # Redis 적용 예정
     @products = Product.where(visible: [true, 1])
     # noinspection RubyResolve
-    if user_signed_in? and current_user.admin
+    if user_signed_in? && current_user.admin
       @products_unvisible = Product.where(visible: [false, 0])
     end
     @products_unvisible = Product.where(visible: [false, 0]) if admin?
   end
-
 end
