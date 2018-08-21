323763045b903c2182bb85a7edc277aeca870c28
limdongjin <geniuslim27@gmail.com>
2018-08-22 04:32:58 +0900

    fixed redis setting error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
-  config.action_mailer.default_url_options = { host: 'ren.2jung.com' }
+  config.action_mailer.default_url_options = { host: '2jung.com' }
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
---
+++ config/initializers/redis.rb
@@ -0,0 +1,15 @@
+# config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/12" }
+rails_root = Rails.root || File.dirname(__FILE__) + '/../..'
+rails_env = Rails.env || 'development'
+redis_config = YAML.load_file(rails_root.to_s + '/config/redis.yml')
+redis_config.merge! redis_config.fetch(Rails.env, {})
+redis_config.symbolize_keys!
+
+if Rails.env == 'development'
+  $redis = Redis.new(host: redis_config[:development]['host'], port: redis_config[:development]['port'])
+else
+  $redis = Redis.new(host: redis_config[:production]['host'], port: redis_config[:production]['port'])
+  print($redis)
+  print(redis_config)
+  print(redis_config[:production]['host'])
+end
