bc3847b6f5f1775ce1e8918ad4d0cf8871f2f866
limdongjin <geniuslim27@gmail.com>
2018-08-23 01:49:42 +0900

    프로필 이미지 추가 완료

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180822154150) do
+ActiveRecord::Schema.define(version: 20180822162347) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -255,6 +255,7 @@ ActiveRecord::Schema.define(version: 20180822154150) do
     t.text "address_text"
     t.text "address_extra"
     t.boolean "admin", default: false
+    t.text "image0"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
