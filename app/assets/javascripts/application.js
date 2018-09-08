77c743dcf299e1cff96dd4a0ef0bcec5697599ed
limdongjin <geniuslim27@gmail.com>
2018-09-09 04:01:51 +0900

    spinner hide

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,26 +17,26 @@
 //= require jquery
 //= require jquery_ujs
 
-$(document).on('turbolinks:load', function () {
-  $('.loaded').show();
-  $('.loading-background').hide();
-  $('.loading-indicator').hide();
-});
-$(document).on('turbolinks:visit', function () {
-    $('.loaded').show();
-    $('.loading-background').hide();
-    $('.loading-indicator').hide();
-});
-$(document).on('turbolinks:request-start', function () {
-  $('.loaded').hide();
-  $('.loading-background').show();
-  $('.loading-indicator').show();
-});
-$(document).on('turbolinks:request-end', function () {
-  $('.loaded').show();
-  $('.loading-background').hide();
-  $('.loading-indicator').hide();
-});
+// $(document).on('turbolinks:load', function () {
+//   $('.loaded').show();
+//   $('.loading-background').hide();
+//   $('.loading-indicator').hide();
+// });
+// $(document).on('turbolinks:visit', function () {
+//     $('.loaded').show();
+//     $('.loading-background').hide();
+//     $('.loading-indicator').hide();
+// });
+// $(document).on('turbolinks:request-start', function () {
+//   $('.loaded').hide();
+//   $('.loading-background').show();
+//   $('.loading-indicator').show();
+// });
+// $(document).on('turbolinks:request-end', function () {
+//   $('.loaded').show();
+//   $('.loading-background').hide();
+//   $('.loading-indicator').hide();
+// });
 function sidebar_click(){
 $(document).ready(function () {
   $('#dismiss, #dismiss-button, .overlay, #exit-button').on('click', function () {
