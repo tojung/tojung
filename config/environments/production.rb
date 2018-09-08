cdae90e82717fafa1dcaa8b63901a43595ac8774
limdongjin <geniuslim27@gmail.com>
2018-09-09 03:31:29 +0900

    utf 8

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,8 @@ Rails.application.configure do
   config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  config.assets.js_compressor = :uglifier
+  #Uglifier.new(harmony: true)
 
   # config.assets.css_compressor = :sass
 
