b3ad7a86c7c1ce3c77d84867934d29e4ea781ceb
limdongjin <30315743+limdongjin@users.noreply.github.com>
2018-09-09 09:49:01 +0900

    Service worker 적용 (#55)
    * Postcode
    
    * Service worker init
    
    * Service worker update
    
    * No CORS 적용
    
    * No CORS 적용
    
    * jserb some update
    
    * Update js erb
    
    * Update js erb
    
    * utf 8
    
    * utf 8
    
    * Url safe
    
    * mainimage ok
    
    * Go
    
    * delete manifest
    
    * spinner hide
    
    * delete spinner
    
    * Product Option cach
    
    * Remove some console log
    
    * Service worker update
    
    * resolve
    
    * Some bug resolve
    
    * Some bug resolve
    eb deploy tototo
    
    * Delete console log
    
    * slick add
    
    * Mainimage
    
    * Service Worker Store Images
    
    * HTML caching 되는 문제 해결
    
    * some change
    
    * Some Refactoring

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,8 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
+  #Uglifier.new(harmony: true)
+
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
