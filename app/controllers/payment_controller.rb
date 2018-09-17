cebf58d537a3093f265d78affeb1f78d752cd9c3
limdongjin <geniuslim27@gmail.com>
2018-09-17 16:16:06 +0900

    Some payment error resolve

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -6,14 +6,14 @@ class PaymentController < ApplicationController
     imp_uid = params['imp_uid']
     parsed_imp = Iamport.payment(imp_uid).parsed_response['response']
     # imp = parsed_imp.reject{ |key, value| !Payment.attribute_names.include?(key)}
-
+    puts parsed_imp['status']
     if parsed_imp['status'] == 'paid'
       payment = Payment.new()
       payment.imp_uid = parsed_imp['imp_uid']
-      parsed_imp.pg_provider = parsed_imp['pg_provider']
-      parsed_imp.amount = parsed_imp['amount']
-      parsed_imp.status = parsed_imp['status']
-      parsed_imp.merchant_uid = parsed_imp['merchant_uid']
+      payment.pg_provider = parsed_imp['pg_provider']
+      payment.amount = parsed_imp['amount']
+      payment.status = parsed_imp['status']
+      payment.merchant_uid = parsed_imp['merchant_uid']
       payment.permission = true
       payment.validation = true
       payment.save
