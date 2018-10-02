82e12bffbd1bdeb31765d78b3fffad4959550e11
limdongjin <geniuslim27@gmail.com>
2018-10-02 20:00:16 +0900

    some error resolve

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,11 +5,11 @@ class HomeController < ApplicationController
   before_action :set_cdn_url, only: %i[index policy privacy brand mypage search]
   # GET '/' 메인 페이지
   def index
-    puts '$'*100
+    puts '$' * 100
     puts request.env['REMOTE_ADDR']
     if params.key?(:sidebar)
       @num = params[:sidebar]
-      @category_list = {'1' => '인권/성평등', '2' => '동물', '3' => '육아/교육', '4' => '안전/환경', '5' => '보건/복지', '6' => '외교/통일/국방', '7' => 'HOT', '8' => 'NEW'}
+      @category_list = { '1' => '인권/성평등', '2' => '동물', '3' => '육아/교육', '4' => '안전/환경', '5' => '보건/복지', '6' => '외교/통일/국방', '7' => 'HOT', '8' => 'NEW' }
       @products = if @num == '7' || @num == '8'
                     @products
                   else
@@ -22,7 +22,7 @@ class HomeController < ApplicationController
     if params[:term] == ''
       @result = false
     elsif params[:term]
-      @result = Product.where('name LIKE ? OR subname LIKE ? OR video_text LIKE ? OR assos LIKE ? OR bill_name LIKE ?', "%#{params[:term]}%",  "%#{params[:term]}%",  "%#{params[:term]}%",  "%#{params[:term]}%",  "%#{params[:term]}%").order('id')
+      @result = Product.where('name LIKE ? OR subname LIKE ? OR video_text LIKE ? OR assos LIKE ? OR bill_name LIKE ?', "%#{params[:term]}%", "%#{params[:term]}%", "%#{params[:term]}%", "%#{params[:term]}%", "%#{params[:term]}%").order('id')
     end
   end
 
@@ -30,7 +30,7 @@ class HomeController < ApplicationController
     @cdn_url = 'https://d1eq7v76s8dt2n.cloudfront.net/'
     @products = Product.where(visible: [true, 1])
     @num = params[:category_id]
-    @category_list = {'1' => '인권/성평등', '2' => '동물', '3' => '육아/교육', '4' => '안전/환경', '5' => '보건/복지', '6' => '외교/통일/국방', '7' => 'HOT', '8' => 'NEW'}
+    @category_list = { '1' => '인권/성평등', '2' => '동물', '3' => '육아/교육', '4' => '안전/환경', '5' => '보건/복지', '6' => '외교/통일/국방', '7' => 'HOT', '8' => 'NEW' }
     @products_categorized = if @num == '7' || @num == '8'
                               @products
                             else
@@ -51,9 +51,22 @@ class HomeController < ApplicationController
 
   def brand; end
 
-  def mypage; end
+  def mypage
+    cal_uproduct_count
+  end
 
   private
+
+  def cal_uproduct_count
+    @ordered_product_count = 0
+    uproducts = []
+    current_user.product_orders.each do |product_order|
+      uproducts.append(product_order.product.id) if uproducts.include? product_order.product.id
+    end
+    @ordered_product_count = uproducts.length
+
+
+  end
 
   def image_read_or_set
     mainimage0 = $redis.get('mainimage0')
@@ -76,12 +89,12 @@ class HomeController < ApplicationController
       @md_link1 = $redis.get('md_link1')
     end
 
-    if @mainimage3 == nil
+    if @mainimage3.nil?
       @mainimage3 = Mainimage.last.image3
-        @mainimage4 = Mainimage.last.image4
+      @mainimage4 = Mainimage.last.image4
 
-        @md_link_0 = Mainimage.last.md_link0
-        @md_link_1 = Mainimage.last.md_link1
+      @md_link_0 = Mainimage.last.md_link0
+      @md_link_1 = Mainimage.last.md_link1
     end
   end
 
