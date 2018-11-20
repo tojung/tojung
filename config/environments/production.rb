54eeadb3ed570f5ec8cd22cb063341bd1fa59fbe
limdongjin <geniuslim27@gmail.com>
2018-11-20 12:45:09 +0900

    devise url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
-  config.action_mailer.default_url_options = { host: '2jung.com' }
+  config.action_mailer.default_url_options = { host: 'tojung.me' }
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
