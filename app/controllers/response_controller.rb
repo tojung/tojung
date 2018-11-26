2922dc229c69165a78cdad11aa951c535bad0bac
limdongjin <geniuslim27@gmail.com>
2018-11-26 23:33:03 +0900

    logginf

--- app/controllers/response_controller.rb
+++ app/controllers/response_controller.rb
@@ -9,7 +9,7 @@ class ResponseController < ApplicationController
     end
     @maker_response.update(response_type: "찬성")
     begin
-      Responselog.create(title: "찬성", maker_response_id: @maker_response.id)
+      Responselog.create(title: "찬성", maker_response_id: @maker_response[0].id)
     rescue
     end
   end
