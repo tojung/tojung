9b12a3ca070a3b43755cfd8cc2a7876ebafe4129
limdongjin <geniuslim27@gmail.com>
2018-12-04 23:23:25 +0900

    ProductDelivery Admin Page || Myorder delivery info

--- app/admin/product_deliveries.rb
+++ app/admin/product_deliveries.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register ProductDelivery do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :product_order, :product, :package, :status, :product_order_detail
 #
 # or
 #
