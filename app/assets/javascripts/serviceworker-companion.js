cf071c54ab5edd451d12eac0b6b631383112e7e0
limdongjin <geniuslim27@gmail.com>
2018-12-06 19:23:10 +0900

    update serviceworker request

--- app/assets/javascripts/serviceworker-companion.js
+++ app/assets/javascripts/serviceworker-companion.js
@@ -45,16 +45,6 @@ navigator.serviceWorker.ready
                                             message: 'Hey!'
                                         };
                                         console.log(body);
-                                        fetch('/push', {
-                                            method: 'POST',
-                                            body: JSON.stringify(body)
-                                        })
-                                            .then((res) => {
-                                                console.log(res)
-                                            })
-                                            .catch((err) => {
-                                                console.log(err)
-                                            })
                                     })
                             })
                     }
