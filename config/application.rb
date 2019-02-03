908164cc784f625d8059055885b2500c7ba5b66a
limdongjin <geniuslim27@gmail.com>
2019-02-03 15:18:18 +0900

    change controller cors

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module T
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: %i[get post options]
       end
     end
     # Settings in config/environments/* take precedence over those specified here.
