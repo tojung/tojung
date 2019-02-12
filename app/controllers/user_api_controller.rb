c6f441a8725ac63eef0b91ea58764a8bba59ef71
limdongjin <geniuslim27@gmail.com>
2019-02-12 23:03:56 +0900

    check user auth route

--- app/controllers/user_api_controller.rb
+++ app/controllers/user_api_controller.rb
@@ -1,6 +1,8 @@
 class UserApiController < ApplicationController
   before_action :authenticate_user_json!
   def check_token
-    render json: { "success": true, "msg": "user is sign_in" }
+    render json: { "success": true,
+                   "msg": "user is sign_in",
+                   user: custom_current_user }
   end
 end
