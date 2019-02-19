94469ee0af49d0ab50365add043fc6427d543739
limdongjin <geniuslim27@gmail.com>
2019-02-20 01:28:37 +0900

    admin makers

--- app/admin/makers.rb
+++ app/admin/makers.rb
@@ -1,15 +1,10 @@
 ActiveAdmin.register Maker do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
 
+  permit_params :image0
+  form title: 'm' do |_f|
+    input 'image_0', label: '메인 이미지'
+    actions
+  end
 end
