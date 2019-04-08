d081c7d20390ba21e537b2c2415ee1f10db660b6
Zinyon <jy2045@sogang.ac.kr>
2019-04-08 20:33:40 +0900

    Revert "subscribe"
    This reverts commit 96e58f63d8c307ad74fd20656f1fe364051129b7.

--- config/routes.rb
+++ config/routes.rb
@@ -42,7 +42,6 @@ Rails.application.routes.draw do
   get 'home/privacy'
   get 'home/mypage'
   get 'home/myorder'
-  get 'home/subscribe'
 
   get 'order_info' => 'order_info#new'
   post 'order_info' => 'order_info#show'
