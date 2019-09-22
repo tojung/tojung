bfd679abda540c23aa4f2d6eadfb1f9ac49a165c
imdongjin <geniuslim27@gmail.com>
2019-09-23 01:52:59 +0900

    feature(order_info_service): show only recent data

--- app/services/order_info_service.rb
+++ app/services/order_info_service.rb
@@ -7,9 +7,11 @@ class OrderInfoService
     @product_order_details = ProductOrderDetail.where(email: @params[:email],
                                                       name: @params[:name],
                                                       phone_num: @params[:phone_num])
+                                               .where("created_at > ?", 90.days.ago)
     @product_orders = []
     @product_order_details.each do |product_order_detail|
       next if product_order_detail.product_order.status == '결제진행중'
+
       @product_orders.push(ProductOrder.find(product_order_detail.product_order_id))
     end
     @product_orders
